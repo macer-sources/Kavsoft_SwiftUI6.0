e53ffd5755ebcdfba93ceab676dbf5f57bddf401
toakan <playtomandjerry>
2024-08-10 22:51:30 +0800

    feat: 自定义收拾(测试未通过)

--- A01-New in SwiftUI-Xcode 16/A01-New in SwiftUI-Xcode 16/A01_New_in_SwiftUI_Xcode_16App.swift
+++ A01-New in SwiftUI-Xcode 16/A01-New in SwiftUI-Xcode 16/A01_New_in_SwiftUI_Xcode_16App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct A01_New_in_SwiftUI_Xcode_16App: App {
     var body: some Scene {
         WindowGroup {
-            
+            GestureRepresentable()
         }
     }
 }
