85efc0d2e10ec02156bddd8d368a0ba86ff1726a
toakan <playtomandjerry>
2024-08-11 15:47:14 +0800

    feat: create project Mesh Gradient & Floating Tab bar
    https://www.youtube.com/watch?v=Y5QnGWOsEmQ&list=PLimqJDzPI-H97tul26Y43ww67oJ49jJ-j&index=4

---
+++ A04-Mesh Gradient & Floating Tab bar/A04-Mesh Gradient & Floating Tab bar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  A04-Mesh Gradient & Floating Tab bar
+//
+//  Created by Kan Tao on 2024/8/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
