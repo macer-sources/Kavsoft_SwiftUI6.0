f5ec20e75dac6a9d3e9516b0166b7c7251bb79ae
toakan <playtomandjerry>
2024-08-11 15:16:22 +0800

    feat: create project
    https://www.youtube.com/watch?v=fuJT6IacatE&list=PLimqJDzPI-H97tul26Y43ww67oJ49jJ-j&index=4

---
+++ A03-Control Center API/A03-Control Center API/A03_Control_Center_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  A03_Control_Center_APIApp.swift
+//  A03-Control Center API
+//
+//  Created by Kan Tao on 2024/8/11.
+//
+
+import SwiftUI
+
+@main
+struct A03_Control_Center_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ A03-Control Center API/A03-Control Center API/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  A03-Control Center API
+//
+//  Created by Kan Tao on 2024/8/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
