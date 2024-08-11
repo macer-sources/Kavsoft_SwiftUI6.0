67be26d93945b4760bbf8222d9e9d7177bf52ae4
toakan <playtomandjerry>
2024-08-11 20:10:07 +0800

    feat: create project - SwiftUI Scroll To Hide Header View

---
+++ A05-SwiftUI Scroll To Hide Header View/A05-SwiftUI Scroll To Hide Header View/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  A05-SwiftUI Scroll To Hide Header View
+//
+//  Created by Kan Tao on 2024/8/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
