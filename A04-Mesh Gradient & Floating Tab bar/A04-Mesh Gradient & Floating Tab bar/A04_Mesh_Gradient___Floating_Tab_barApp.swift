4937d46a0b06cb00b86fa693d030dfdd7fe56a88
toakan <playtomandjerry>
2024-08-11 15:53:13 +0800

    feat: MeshGradient

--- A04-Mesh Gradient & Floating Tab bar/A04-Mesh Gradient & Floating Tab bar/A04_Mesh_Gradient___Floating_Tab_barApp.swift
+++ A04-Mesh Gradient & Floating Tab bar/A04-Mesh Gradient & Floating Tab bar/A04_Mesh_Gradient___Floating_Tab_barApp.swift
@@ -3,7 +3,7 @@
 import SwiftUI
 
 @main
-struct ___PACKAGENAME:identifier___App: App {
+struct AppProject: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
--- A04-Mesh Gradient & Floating Tab bar/A04-Mesh Gradient & Floating Tab bar/ContentView.swift
+++ A04-Mesh Gradient & Floating Tab bar/A04-Mesh Gradient & Floating Tab bar/ContentView.swift
@@ -9,15 +9,65 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        MeshGradient(width: 3,
+                     height: 3,
+                     points:[
+                        .init(0,0),
+                        .init(0.5, 0),
+                        .init(1, 0),
+                        .init(0, 0.5),
+                        .init(0.5, 0.5),
+                        .init(1, 0.5),
+                        .init(0, 1),
+                        .init(0.5, 1),
+                        .init(1, 1)
+                     ],
+                     colors: [
+                        .red, .orange,.pink,
+                        .pink, .green, .yellow,
+                        .indigo, .mint, .cyan
+                     ])
+        .overlay {
+            GeometryReader {
+                let size = $0.size
+                
+                VStack(spacing: 0) {
+                    HStack(spacing: 0) {
+                        CircleView()
+                        CircleView()
+                        CircleView(true)
+                    }
+                    HStack(spacing: 0) {
+                        CircleView()
+                        CircleView()
+                        CircleView(true)
+                    }
+                    .frame(maxHeight: .infinity)
+                    
+                    HStack(spacing: 0) {
+                        CircleView()
+                        CircleView()
+                        CircleView(true)
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
+
+
+extension ContentView {
+    @ViewBuilder
+    func CircleView(_ isLast: Bool = false ) -> some View {
+        Circle()
+            .fill(.black)
+            .frame(width: 10, height: 10)
+        if !isLast {
+            Spacer(minLength: 0)
+        }
+    }
+}
+
 
 #Preview {
     ContentView()
