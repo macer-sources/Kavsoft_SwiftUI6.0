6b17f2ba1a1d2172ccaa0a3939edb0f6d8d95cf5
toakan <playtomandjerry>
2024-09-22 19:29:20 +0800

    feat: A08- init project

---
+++ A08-SwiftUI Draggable Tab bar/A08-SwiftUI Draggable Tab bar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  A08-SwiftUI Draggable Tab bar
+//
+//  Created by Kan Tao on 2024/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
