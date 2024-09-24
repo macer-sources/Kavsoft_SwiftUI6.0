04c5bc28d49d16c4e711a3567f5dbcc87a7595c5
toakan <playtomandjerry>
2024-09-24 21:47:57 +0800

    feat: A17 init project

---
+++ A17.SwiftUI Floating Bottom Sheets/A17.SwiftUI Floating Bottom Sheets/A17_SwiftUI_Floating_Bottom_SheetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  A17_SwiftUI_Floating_Bottom_SheetsApp.swift
+//  A17.SwiftUI Floating Bottom Sheets
+//
+//  Created by Kan Tao on 2024/9/24.
+//
+
+import SwiftUI
+
+@main
+struct A17_SwiftUI_Floating_Bottom_SheetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
