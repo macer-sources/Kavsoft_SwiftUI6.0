1ba495b507bbd01189d69dc88644172f4e0840d7
toakan <playtomandjerry>
2024-09-28 13:34:22 +0800

    feat: init project
    https://www.youtube.com/watch?v=p1nN9eFOPNQ

---
+++ A33.Auto Scrolling Infinite Carousel - iOS 18/A33.Auto Scrolling Infinite Carousel - iOS 18/A33_Auto_Scrolling_Infinite_Carousel___iOS_18App.swift
@@ -0,0 +1,12 @@
+//___FILEHEADER___
+
+import SwiftUI
+
+@main
+struct Auto_Scrolling_Infinite_Carousel: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ A33.Auto Scrolling Infinite Carousel - iOS 18/A33.Auto Scrolling Infinite Carousel - iOS 18/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  A33.Auto Scrolling Infinite Carousel - iOS 18
+//
+//  Created by Kan Tao on 2024/9/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
