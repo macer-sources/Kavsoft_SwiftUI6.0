a6b1fd4b4a3aade8eb495530166efa050d568ff1
toakan <playtomandjerry>
2024-10-06 12:06:35 +0800

    A20: init project

---
+++ A20.Floating Tab Bar iOS/A20.Floating Tab Bar iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  A20.Floating Tab Bar iOS
+//
+//  Created by Kan Tao on 2024/10/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
