82c968680a4aa81f605a9588298649f0d8d4ea77
toakan <playtomandjerry>
2024-10-06 17:59:36 +0800

    A21. init project

---
+++ A21.SwiftUI-App-Theme-Switcher-iOS-17-18/A21.SwiftUI-App-Theme-Switcher-iOS-17-18/A21_SwiftUI_App_Theme_Switcher_iOS_17_18App.swift
@@ -0,0 +1,17 @@
+//
+//  A21_SwiftUI_App_Theme_Switcher_iOS_17_18App.swift
+//  A21.SwiftUI-App-Theme-Switcher-iOS-17-18
+//
+//  Created by Kan Tao on 2024/10/6.
+//
+
+import SwiftUI
+
+@main
+struct A21_SwiftUI_App_Theme_Switcher_iOS_17_18App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ A21.SwiftUI-App-Theme-Switcher-iOS-17-18/A21.SwiftUI-App-Theme-Switcher-iOS-17-18/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  A21.SwiftUI-App-Theme-Switcher-iOS-17-18
+//
+//  Created by Kan Tao on 2024/10/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
