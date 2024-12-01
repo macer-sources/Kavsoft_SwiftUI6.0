f53ba819967b432f64d0be2a6f6751ade428cc91
toakan <playtomandjerry>
2024-12-01 20:17:54 +0800

    A40. init project

---
+++ A40.App-Wide Overlays | SwiftUI/A40.App-Wide Overlays | SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  A40.App-Wide Overlays | SwiftUI
+//
+//  Created by Kan Tao on 2024/12/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
