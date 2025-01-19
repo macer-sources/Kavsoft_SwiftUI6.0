cea0e9e2c0ffdef905d5cd01ce74fb2632de090b
niehulai <main@maindeMacBook-Pro.local>
2025-01-19 12:29:16 +0800

    A39.init project
    https://www.youtube.com/watch?v=nQr6d9_yeG0&list=PLimqJDzPI-H97tul26Y43ww67oJ49jJ-j&index=39&t=16s

---
+++ A39.SwiftUI Toasts Xcode 16/A39.SwiftUI Toasts Xcode 16/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  A39.SwiftUI Toasts Xcode 16
+//
+//  Created by main on 2025/1/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
