fc9d0cfe048f368f684ede7b4c8d09b6ac5b323b
toakan <playtomandjerry>
2025-02-03 12:43:58 +0800

    feat: A68.init project

---
+++ A68.Manual View Orientation Control/A68.Manual View Orientation Control/A68_Manual_View_Orientation_ControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  A68_Manual_View_Orientation_ControlApp.swift
+//  A68.Manual View Orientation Control
+//
+//  Created by Kan Tao on 2025/2/3.
+//
+
+import SwiftUI
+
+@main
+struct A68_Manual_View_Orientation_ControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ A68.Manual View Orientation Control/A68.Manual View Orientation Control/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  A68.Manual View Orientation Control
+//
+//  Created by Kan Tao on 2025/2/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
