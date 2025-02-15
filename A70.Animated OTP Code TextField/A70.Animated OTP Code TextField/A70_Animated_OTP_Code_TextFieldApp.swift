f4216d445ecf60dec2091cbee46fc412111f40d6
fredevteam <playtomandjerry@outlook.com>
2025-02-15 21:20:21 +0800

    A70.init project

---
+++ A70.Animated OTP Code TextField/A70.Animated OTP Code TextField/A70_Animated_OTP_Code_TextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  A70_Animated_OTP_Code_TextFieldApp.swift
+//  A70.Animated OTP Code TextField
+//
+//  Created by main on 2025/2/15.
+//
+
+import SwiftUI
+
+@main
+struct A70_Animated_OTP_Code_TextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
